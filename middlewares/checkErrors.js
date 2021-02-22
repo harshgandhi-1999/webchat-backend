b42030d2dffe14f6afb2ff61888e83d9eb65d0d5
harsh gandhi <harshgandhi043@gmail.com>
2021-02-23 01:48:49 +0530

    removed consoles

--- middlewares/checkErrors.js
+++ middlewares/checkErrors.js
@@ -2,7 +2,6 @@ const { validationResult } = require("express-validator");
 
 const checkErrors = (req, res, next) => {
   const errors = validationResult(req);
-  // console.log(errors);
   if (!errors.isEmpty()) {
     return res.status(422).json({
       message: errors.array()[0].msg,
