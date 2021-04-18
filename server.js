fe5be1badab5f359cece745fbf30049c050b0103
harsh gandhi <harshgandhi043@gmail.com>
2021-04-18 23:06:52 +0530

    added cors in socket io

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Accept", "Authorization"],
   },
 });
 
