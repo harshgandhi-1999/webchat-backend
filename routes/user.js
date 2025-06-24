61421de2a8457cb68f870aff89fa99596f055cb3
harsh-the-innovator <harshgandhi043@gmail.com>
2025-06-24 17:30:05 +0530

    Contact add and get contact list routes fixed

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { check } = require("express-validator");
+const { body, check } = require("express-validator");
 
 const {
   addContact,
@@ -12,22 +12,21 @@ const checkErrors = require("../middlewares/checkErrors");
 const isAuthorized = require("../middlewares/isAuthorized");
 
 router.post(
-  "/contact/add/:userId",
+  "/contact/add/",
   [
-    check("contactNo", "Contact no. should be of 10 digits").matches(
-      /^[6-9][0-9]{9}/,
-      "i"
-    ),
-    check("name", "Name is required").trim().isLength({ min: 1 }),
+    body("contactId").isMongoId().withMessage("Field error: Invalid contactId"),
+    check("aliasName", "Field error: aliasName is required")
+      .trim()
+      .isLength({ min: 1 }),
   ],
   checkErrors,
   isAuthorized,
   addContact
 );
-router.get("/contact/all/:userId", isAuthorized, getAllContacts);
+router.get("/contact/", isAuthorized, getAllContacts);
 
-router.get("/chatlist/:userId", isAuthorized, getChatList);
+// router.get("/chatlist/:userId", isAuthorized, getChatList);
 
-router.put("/chatlist/:userId", isAuthorized, updateChatList);
+// router.put("/chatlist/:userId", isAuthorized, updateChatList);
 
 module.exports = router;
